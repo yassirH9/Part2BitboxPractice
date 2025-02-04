bb27a3f6ed37959c8c963c859d43fa06c5355a0a
yassirH9 <yassirch2001ch@gmail.com>
2025-02-04 15:55:25 +0000

    mayor changes in stylesheets of components added scss files for all auto generated css file of react added page level component /items ItemList

--- client/bb2pract-test-client/src/App.js
+++ client/bb2pract-test-client/src/App.js
@@ -1,13 +1,14 @@
-import './styles/App.css';
+import './styles/App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import LoginForm from './components/LoginFrom';
-
+import LoginForm from './pages/LoginFrom';
+import ItemView from './pages/ItemView';
 function App() {
   return (  
     <Router>
     <Routes>
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/items" element={<ItemView/>}/>
       {/* Add other routes here */}
     </Routes>
   </Router>
